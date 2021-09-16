20ff632d618ab1667718583ba7077f009e4cf59a
Federico Palma <fedepal9@gmail.com>
2021-09-16 11:54:57 +0200

    Exercise 4 Complete

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,10 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...secondArray) {
+    for (let i = 0; i < secondArray.length; i++) {
+        if (array.includes(secondArray[i])) {
+            array.splice(array.indexOf(secondArray[i]), 1)
+        }
+    }
+    return array
 };
 
 // Do not edit below this line
