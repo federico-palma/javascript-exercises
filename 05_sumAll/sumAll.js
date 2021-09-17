fe3fdd11f7723f6c63d6f8b241cadb468eda3cdd
Federico Palma <fedepal9@gmail.com>
2021-09-17 09:46:48 +0200

    Exercise 5 Complete

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    let sum = 0
+    let list = [num1, num2]
+    
+    if (typeof(num1) !== "number" || typeof(num2) !== "number" || num1 < 0 || num2 < 0) {
+        sum = "ERROR"
+    } else {
+        list.sort((a, b) => a - b);
+        for (let i = list[0]; i < list[1]+1; i++) {
+            sum += i   
+        }
+    }
+    return sum
 };
 
 // Do not edit below this line
