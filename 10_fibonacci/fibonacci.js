6fdc595708b40f9e56a327969963877e4b0de6aa
Federico Palma <fedepal9@gmail.com>
2021-09-19 11:50:48 +0200

    Exercise 10 Complete

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    let fiboArray = [1, 1];
+    let newEntry;
+    if (num < 0) {
+        return "OOPS"
+    } else {
+        for (let i = 0; i < num; i++) {
+            newEntry = fiboArray[fiboArray.length - 1] + fiboArray[fiboArray.length - 2];
+            fiboArray.push(newEntry);
+        }
+    }
+    return fiboArray[num - 1]
 };
 
 // Do not edit below this line
