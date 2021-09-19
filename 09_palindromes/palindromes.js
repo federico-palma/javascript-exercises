b31b29622636b7a78caecacd9c4edfff7c2ed65f
Federico Palma <fedepal9@gmail.com>
2021-09-19 11:28:08 +0200

    Exercise 9 Complete

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    let s1 = string.match(/[^_\W]+/g).join('').toLowerCase();
+    let s2 = s1.split("").reverse().join("");
+    if (s1 == s2) {
+        return true
+    } else {
+        return false
+    }
 };
 
 // Do not edit below this line
