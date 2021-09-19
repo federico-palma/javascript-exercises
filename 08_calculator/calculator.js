50fba42eac1459e8b2066ff20fccd409db8a01f7
Federico Palma <fedepal9@gmail.com>
2021-09-19 11:19:17 +0200

    Exercise 8 Complete

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  let totalSum = 0
+  for (let i = 0; i < array.length; i++) {
+    totalSum += array[i];
+  }
+  return totalSum
 };
 
-const multiply = function() {
-
+const multiply = function(num) {
+  let product = 1
+  for (let i = 0; i < num.length; i++) {
+    product *= num[i];
+  }
+  return product
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let product = 1;
+  for (let i = num; i > 0; i--) {
+    product *= i
+  }
+  return product;
 };
 
 // Do not edit below this line
