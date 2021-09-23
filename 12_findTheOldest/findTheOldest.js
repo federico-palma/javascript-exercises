7563acf8aeeb4d58ccacf2db21c916017d8b5a7f
Federico Palma <fedepal9@gmail.com>
2021-09-23 11:21:37 +0200

    Exercise 12 Complete

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(array) {
+    let currentYear = new Date().getFullYear();
+    let age;
+    for (let i = 0; i < array.length; i++) {
+        if (array[i].yearOfDeath == undefined) {
+            age = currentYear - array[i].yearOfBirth;
+        } else {
+            age = array[i].yearOfDeath - array[i].yearOfBirth;
+        }
+        array[i].age = age;
+    }
+    array.sort((a, b) => a.age - b.age);
+    return array[array.length - 1]
 };
 
 // Do not edit below this line
