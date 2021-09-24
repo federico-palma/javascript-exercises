049791c8b8bb490a74115833fe586e67831780ac
Federico Palma <fedepal9@gmail.com>
2021-09-24 10:58:23 +0200

    Exercise 13 Complete

--- 13_caesar/caesar.js
+++ 13_caesar/caesar.js
@@ -1,6 +1,54 @@
-const caesar = function() {
+// const caesar = function(string, shift) {
+//     const upperCaseAlph = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
+//     const lowerCaseAlph = 'abcdefghijklmnopqrstuvwxyz'.split('');
+//     let newString = ''
 
-};
+//     for (let i = 0; i < string.length; i++) {
+//         let 
+//         if (upperCaseAlph.includes(string[i])) {
+//             newString += upperCaseAlph[upperCaseAlph.indexOf(string[i]) + (shift % upperCaseAlph.length)]
+//         } else if (lowerCaseAlph.includes(string[i])){
+//             newString += lowerCaseAlph[lowerCaseAlph.indexOf(string[i]) + (shift % upperCaseAlph.length)]
+//         } else {
+//             newString += string[i]
+//         }
+        
+//     }
+//     return newString
+// };
+
+// function outOfBounds(num) {
+//     if (num > 0) {
+//         while (num > 26) {num - 26};
+//     } else {
+//         while (num < 26) {num + 26};
+//     }
+//     return num
+//     }
+// }
+
+const caesar = function(string, shift) {
+    return string
+      .split("")
+      .map(char => shiftChar(char, shift))
+      .join("");
+  };
+  
+  const codeSet = code => (code < 97 ? 65 : 97);
+  
+
+  const mod = (n, m) => (n % m + m) % m;
+  
+  const shiftChar = (char, shift) => {
+    const code = char.charCodeAt();
+  
+    if ((code >= 65 && code <= 90) || (code >= 97 && code <= 122)) {
+      return String.fromCharCode(
+        mod(code + shift - codeSet(code), 26) + codeSet(code)
+      );
+    }
+    return char;
+  };
 
 // Do not edit below this line
 module.exports = caesar;
